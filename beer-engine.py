44cde0622becbdb22607d8c92545378824de5070
John Bogil <johncbogil@gmail.com>
2019-05-03 22:01:07 -0400

    using beer unique csv

--- beer-engine.py
+++ beer-engine.py
@@ -9,11 +9,10 @@ import turicreate as tc
 from flask import send_file
 import json
 
-
 app = Flask(__name__)
 
 # Read beers from csv
-beer2 = pd.read_csv('beer2.csv')
+beer2 = pd.read_csv('beer_uniques.csv')
 #Load model
 beer_model = tc.load_model("beer_model")
 print("#Load and read beers from csv")
@@ -28,13 +27,16 @@ def predict():
     try:
         jsonDump = json.dumps(request.json)
         input_test = pd.DataFrame(pd.read_json(jsonDump)) #JSON input from user
+        
+        input_test['beer_beerid'] = pd.DataFrame(beer2.loc[beer2['beer_name'].isin(input_test['beer_name']), 'beer_beerid']) #Obtain beer id for beer name given by user
     
-        input_test['beer_beerid'] = pd.DataFrame(beer2.loc[beer2['beer_name'].isin(input_test['beer_name']), 'beer_beerid'].unique()).astype('int64') #Obtain beer id for beer name given by user
-        
         predict_frame = tc.SFrame(input_test) #Convert user input dataframe to SFrame
+
+        predict_frame['beer_beerid'] = predict_frame['beer_beerid'].astype(int) #input_test beerid gets converted to float
+
         beer_recs = pd.DataFrame(beer_model.recommend(predict_frame['userId'], new_observation_data = predict_frame)) #Predict new beers for user and convert to dataframe
-        
         beer_recs_final = beer_recs.merge(beer2[['beer_name','beer_beerid','beer_abv','beer_style']], on='beer_beerid').drop_duplicates(['beer_beerid']) #Join predictions to beer3 to obtain additional information
+
         beer_recs_final = beer_recs_final[['beer_name','beer_abv','beer_style']].to_json(orient='records') #Convert desired output to json for iOS output
         
         return beer_recs_final  
